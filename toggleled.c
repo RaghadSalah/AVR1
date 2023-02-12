e5a5e578cfe0934d73a47af96d88f873a454510c
RaghadSalah <104818172+RaghadSalah@users.noreply.github.com>
2023-02-12 15:09:57 +0200

    second

--- toggleled.c
+++ toggleled.c
@@ -6,10 +6,9 @@ int main(void)
 DDRA = 0xff;
 while(1)
  {
-DDRA |= (1<<LED_PIN);
-PORTA |= (1<<LED_PIN);
-_delay_ms(10000);
-PORTA &= ~(1<<LED_PIN);
+PORTA = 0b00000001 ;
+_delay_ms(1000);
+PORTA =0;
 _delay_ms(1000);
 }
 }
